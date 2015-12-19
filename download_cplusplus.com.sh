72da37f2dbcbcd4e3372c21d875aafd97b77de4d
Krzysztof Małysa <krzyk240@gmail.com>
2015-12-19 23:27:19 +0100

    New file: download_cplusplus.com.sh

---
+++ download_cplusplus.com.sh
@@ -0,0 +1,1 @@
+./sd cplusplus.com -i cplusplus.com/forum -i cplusplus.com/search.do -i cplusplus.com/contact.do -i cplusplus.com/user/access.cgi -i cplusplus.com/user/profile.cgi -i cplusplus.com/user/pm.cgi -i cplusplus.com/token.do -i cplusplus.com/user/admin.cgi $@
